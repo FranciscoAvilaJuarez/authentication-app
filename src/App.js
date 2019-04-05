f92c64d368f796e388dece9cc5471b8509997719
FranciscoAvilaJuarez <Franciscosalomonavilajuarez@gmail.com>
2019-04-05 15:13:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//*set up router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+//*imports
+import Home from './components/pages/home';
+import Navbar from './components/layout/Navbar';
+import Staff from './components/pages/staff';
+
 import './App.css';
 
+//* toggle switch
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<div className="Container">
+						<Route path="/" exact={true} component={Home} />
+						<Route path="/staff" exact={true} component={Staff} />
+					</div>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
